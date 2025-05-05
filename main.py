8848983977da9c73e8c8b571c383ada7b999f256
Vikash <phanivikash2@gmail.com>
2025-05-05 22:31:59 +0530

    Adding LinkedIN

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import streamlit as st
 st.set_page_config(layout="wide")
 col1 , col2=st.columns(2)
 with col1:
-    st.image('images/phani_vikash.jpeg',width=350)
+    st.image('images/phani_vikash.jpeg',width=300)
 with col2:
     st.title("\t Kote Phani Vikash")
 
